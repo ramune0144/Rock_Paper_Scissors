9f31979855ae4eadc3996e541e6ad008a3a08116
naruepon sowchai <naruepon.sao@ku.th>
2022-03-29 22:26:55 +0700

    add alert

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import Color from "./src/Context/Color";
 import HomeScreen from './src/Screens/HomeScreen';
 import SelectScreen from './src/Screens/SelectScreen';
 import IngameScreen from './src/Screens/IngameScreen';
@@ -23,18 +23,24 @@ import ResultScreen from './src/Screens/ResultScreen';
 
 //   );
 // }
+const defaultStackNavOption = {
+  headerStyle: { backgroundColor: Color.primaryColor },
+
+
+
+}
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Select" component={SelectScreen} />
-        <Stack.Screen name="Ingame" component={IngameScreen} />
-        <Stack.Screen name="Compile" component={CompileScreen} />
-        <Stack.Screen name="Result" component={ResultScreen} />
+      <Stack.Navigator screenOptions={defaultStackNavOption}>
+        <Stack.Screen name="Home"  options={{headerLeft: () => <></>,}} component={HomeScreen} />
+        <Stack.Screen name="Select" options={{headerLeft: () => <></>,}} component={SelectScreen} />
+        <Stack.Screen name="Ingame" options={{headerLeft: () => <></>,}} component={IngameScreen} />
+        <Stack.Screen name="Compile" options={{headerLeft: () => <></>,}} component={CompileScreen} />
+        <Stack.Screen name="Result" options={{headerLeft: () => <></>,}}component={ResultScreen} />
       </Stack.Navigator>
     </NavigationContainer>
     
